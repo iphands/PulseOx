1b0aa682d176d819361d543527ae0b0aae30eb31
Ian Page Hands <iphands@gmail.com>
2010-02-08 12:15:18 -0500

    FileListener.java: dont erase the full gc, just blank x + 1

--- workspace/PulseOx/src/org/ahands/ian/pulseox/FileListener.java
+++ workspace/PulseOx/src/org/ahands/ian/pulseox/FileListener.java
@@ -101,6 +101,10 @@ public class FileListener implements Runnable {
 
 					if (x >= 200) {
 						x = 0;
+						gc.setForeground(new Color(display, 0, 0, 0));
+						gc.drawLine(0, 0, 0, 127);
+						gc.drawLine(1, 0, 1, 127);
+						gc.setForeground(new Color(display, 0, 255, 0));
 						// gc.fillRectangle(canvasRect);
 					} else {
 						// gc.drawPoint(x, waveY);
@@ -155,8 +159,6 @@ public class FileListener implements Runnable {
 				} else {
 					System.out.println("debug: waiting for control character ("
 							+ controlInt + ")");
-
-					deviceReader.close();
 				}
 
 			} catch (IOException e) {
